bd18ab618bcfab1ecc4832635c42c374a668436d
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-12-09 17:29:28 -0500

    emails js try

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ ham_menu.addEventListener("click", () => {
 menu_link.addEventListener("click", () => {
   menu_link.style.display = "none";
 });
+
+// emailjs
+
+function senMail() {
+  console.log("sex");
+  emailjs.init("UqxJU2Ws0Yc0AT-1U");
+  var params = {
+    to_name: document.getElementById("name").value,
+    reply_to: document.getElementById("email").value,
+  };
+  emailjs.send(
+    "service_3qjriwh",
+    "template_ydicqt9",
+    params,
+    "UqxJU2Ws0Yc0AT-1U"
+  );
+}
